036ac21fd8c1360d40c4fbafd36e38347710cf05
Themba Mtshelwane <Thembamm3@gmail.com>
2023-08-30 13:04:26 +0200

    Student Info: Added a catch condition for no data

--- src/pages/info/[topicID]/index.tsx
+++ src/pages/info/[topicID]/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { GetServerSidePropsContext } from 'next';
+import React, { useEffect, useRef, useState } from 'react'
+import { GetServerSidePropsContext } from 'next'
 import {
   Box,
   Heading,
@@ -12,51 +12,61 @@ import {
   Th,
   Td,
   TableContainer,
-} from '@chakra-ui/react';
-import { DownloadTableExcel } from 'react-export-table-to-excel';
+  Text,
+} from '@chakra-ui/react'
+import { DownloadTableExcel } from 'react-export-table-to-excel'
 
-import { getAllUsers } from '../../api/UserData';
-import PageContent from '@/src/components/layout/PageContent';
+import { getAllUsers } from '../../api/UserData'
+import PageContent from '@/src/components/layout/PageContent'
 
 type StudentResult = {
-  email: string;
-  outcome: string;
-};
+  email: string
+  outcome: string
+}
 
 type QuizPageProps = {
-  userData: any[];
-  name: string;
-  quizHistory: any[];
-};
+  userData: any[]
+  name: string
+  quizHistory: any[]
+}
 
 type HistoryEntry = {
-  studentID: string;
-  results: { result: string }[];
-};
+  studentID: string
+  results: { result: string }[]
+}
 
-const QuizPage: React.FC<QuizPageProps> = ({name, quizHistory }) => {
-  const [studentResults, setStudentResults] = useState<StudentResult[]>([]);
-  const [filename, setFilename] = useState<string>(`${name} quiz - student info`);
-  const [show, setShow] = useState<boolean>(false);
-  const tableRef = useRef(null);
+const QuizPage: React.FC<QuizPageProps> = ({ name, quizHistory }) => {
+  const [studentResults, setStudentResults] = useState<StudentResult[]>([])
+  const [filename, setFilename] = useState<string>(
+    `${name} quiz - student info`
+  )
+  const [show, setShow] = useState<boolean>(false)
+  const tableRef = useRef(null)
 
   useEffect(() => {
-    const newStudentResults = quizHistory.map((history: HistoryEntry) => {
-      const correctCount = history.results.filter(
-        (outcome: { result: string }) => outcome.result === 'correct'
-      ).length;
-      const percentage = ((correctCount / (history.results.length - 1)) * 100).toFixed(2);
-      return { email: history.studentID, outcome: percentage };
-    });
-    setStudentResults(newStudentResults);
-    setShow(true);
-  }, []);
+    if (quizHistory.length != 0) {
+      const newStudentResults = quizHistory.map((history: HistoryEntry) => {
+        const correctCount = history.results.filter(
+          (outcome: { result: string }) => outcome.result === 'correct'
+        ).length
+        const percentage = (
+          (correctCount / (history.results.length - 1)) *
+          100
+        ).toFixed(2)
+        return { email: history.studentID, outcome: percentage }
+      })
+      setStudentResults(newStudentResults)
+      setShow(true)
+    }
+  }, [])
 
   return (
     <>
       <PageContent>
         <Box m={2} p={5}>
-          <Heading m={2} p={5}>Student Information for {name} Quiz</Heading>
+          <Heading m={2} p={5}>
+            Student Information for {name} Quiz
+          </Heading>
 
           <Link href="/dashboard">
             <Button
@@ -67,7 +77,8 @@ const QuizPage: React.FC<QuizPageProps> = ({name, quizHistory }) => {
                 transform: 'scale(0.98)',
               }}
               _focus={{
-                boxShadow: '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+                boxShadow:
+                  '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
                 bg: ' #618fd9',
                 color: 'white',
               }}
@@ -76,76 +87,84 @@ const QuizPage: React.FC<QuizPageProps> = ({name, quizHistory }) => {
             </Button>
           </Link>
 
-          <Box m={2}>
-            <DownloadTableExcel
-              filename={filename}
-              sheet="students"
-              currentTableRef={tableRef.current}
-            >
-              <Button
-                color="black"
-                border="2px solid #265e9e"
-                width="100%"
-                _active={{
-                  transform: 'scale(0.98)',
-                }}
-                _focus={{
-                  boxShadow: '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-                  bg: ' #618fd9',
-                  color: 'white',
-                }}
+          {quizHistory.length != 0 ? (
+            <Box m={2}>
+              <DownloadTableExcel
+                filename={filename}
+                sheet="students"
+                currentTableRef={tableRef.current}
               >
-                Download Data
-              </Button>
-            </DownloadTableExcel>
+                <Button
+                  color="black"
+                  border="2px solid #265e9e"
+                  width="100%"
+                  _active={{
+                    transform: 'scale(0.98)',
+                  }}
+                  _focus={{
+                    boxShadow:
+                      '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+                    bg: ' #618fd9',
+                    color: 'white',
+                  }}
+                >
+                  Download Data
+                </Button>
+              </DownloadTableExcel>
 
-            <TableContainer>
-              <Table variant="simple" ref={tableRef}>
-                <Thead>
-                  <Tr>
-                    <Th>Student Email</Th>
-                    <Th>Results %</Th>
-                  </Tr>
-                </Thead>
+              <TableContainer>
+                <Table variant="simple" ref={tableRef}>
+                  <Thead>
+                    <Tr>
+                      <Th>Student Email</Th>
+                      <Th>Results %</Th>
+                    </Tr>
+                  </Thead>
 
-                <Tbody>
-                  {studentResults.length !== 0 &&
-                    studentResults.map((studentData, index) => (
-                      <Tr key={studentData.email}>
-                        <Td>{studentData.email}</Td>
-                        <Td>{studentData.outcome}</Td>
-                      </Tr>
-                    ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
+                  <Tbody>
+                    {studentResults.length !== 0 &&
+                      studentResults.map((studentData, index) => (
+                        <Tr key={studentData.email}>
+                          <Td>{studentData.email}</Td>
+                          <Td>{studentData.outcome}</Td>
+                        </Tr>
+                      ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
 
-            <Link href="/dashboard">
-              <Button
-                color="black"
-                border="2px solid #265e9e"
-                width="100%"
-                _active={{
-                  transform: 'scale(0.98)',
-                }}
-                _focus={{
-                  boxShadow: '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-                  bg: ' #618fd9',
-                  color: 'white',
-                }}
-              >
-                Back
-              </Button>
-            </Link>
-          </Box>
+              <Link href="/dashboard">
+                <Button
+                  color="black"
+                  border="2px solid #265e9e"
+                  width="100%"
+                  _active={{
+                    transform: 'scale(0.98)',
+                  }}
+                  _focus={{
+                    boxShadow:
+                      '0 0 1px 2px rgba(97, 143, 217, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+                    bg: ' #618fd9',
+                    color: 'white',
+                  }}
+                >
+                  Back
+                </Button>
+              </Link>
+            </Box>
+          ) : (
+            <Box>
+              <Text>Data is not available. Students must answer the quiz</Text>
+            </Box>
+          )}
         </Box>
       </PageContent>
     </>
-  );
-};
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  return getAllUsers(context);
+  )
 }
 
-export default QuizPage;
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return getAllUsers(context)
+}
+
+export default QuizPage
